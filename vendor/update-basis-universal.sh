98714e850accbdc536ef90892d3688f3c3d44a2f
Luo Zhihao <luo_zhihao@outlook.com>
2026-03-30 14:58:45 +0800

    Migrate basis_universal to non-lfs

--- vendor/update-basis-universal.sh
+++ vendor/update-basis-universal.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+git clone --depth 1 https://github.com/BinomialLLC/basis_universal --branch v2_1_0 basis_universal_repo
+
 rm -r ./basis_universal
 mkdir ./basis_universal
 
-git clone --depth 1 https://github.com/BinomialLLC/basis_universal --branch v2_1_0 basis_universal_repo
 cp -r ./basis_universal_repo/transcoder/ ./basis_universal/
 cp -r ./basis_universal_repo/zstd/ ./basis_universal/
 cp ./basis_universal_repo/LICENSE ./basis_universal/
