a3bc37d646f4fa26dd71f695b4a493bd21d64730
Luo Zhihao <luo_zhihao@outlook.com>
2026-03-30 14:13:37 +0800

    Add snapshot tests

--- crates/basisu_sys/src/native.rs
+++ crates/basisu_sys/src/native.rs
@@ -12,3 +12,34 @@ pub use crate::transcoding::c_ktx2_transcoder_get_r_width as ktx2_transcoder_get
 pub use crate::transcoding::c_ktx2_transcoder_new as ktx2_transcoder_new;
 pub use crate::transcoding::c_ktx2_transcoder_transcode_image_get_info as ktx2_transcoder_transcode_image_get_info;
 pub use crate::transcoding::c_ktx2_transcoder_transcode_image_write_buffer as ktx2_transcoder_transcode_image_write_buffer;
+
+#[cfg(test)]
+pub unsafe fn ktx2_transcoder_transcode_image_alloc_dst(
+    transcoder: *mut crate::transcoding::Transcoder,
+    data: alloc::vec::Vec<u8>,
+    supported_compressed_formats: crate::SupportedTextureCompressionMethods,
+    channel_type_hint: crate::ChannelType,
+    force_transcode_target: crate::TranscodedTextureFormat,
+) -> bool {
+    unsafe {
+        crate::transcoding::c_ktx2_transcoder_transcode_image_alloc_dst(
+            transcoder,
+            data.as_ptr(),
+            u32::try_from(data.len()).unwrap(),
+            supported_compressed_formats,
+            channel_type_hint,
+            force_transcode_target,
+        )
+    }
+}
+
+#[cfg(test)]
+pub unsafe fn ktx2_transcoder_get_r_dst_buf(
+    transcoder: *mut crate::transcoding::Transcoder,
+) -> alloc::vec::Vec<u8> {
+    let ptr = unsafe { crate::transcoding::c_ktx2_transcoder_get_r_dst_buf(transcoder) };
+    let len = unsafe { crate::transcoding::c_ktx2_transcoder_get_r_dst_buf_len(transcoder) };
+    let mut ret = alloc::vec![0; len as usize];
+    unsafe { core::ptr::copy_nonoverlapping(ptr, ret.as_mut_ptr(), len as usize) };
+    ret
+}
--- encode_assets.sh
+++ encode_assets.sh
@@ -3,7 +3,7 @@
 $BASISU_TOOL -etc1s -mipmap ./original_assets/alpha0.png -output_file ./assets/alpha0_etc1s_mips.basisu.ktx2
 $BASISU_TOOL -uastc_hdr_4x4 ./original_assets/Desk_fixed_6x6.exr -output_file ./assets/desk_uastc_hdr_4x4.basisu.ktx2
 $BASISU_TOOL -uastc_hdr_6x6 -mipmap ./original_assets/Desk_fixed_6x6.exr -output_file ./assets/desk_uastc_hdr_6x6_mips.basisu.ktx2
-$BASISU_TOOL -xuastc_ldr_8x8 -mipmap -cubemap ./original_assets/skybox/right.jpg ./original_assets/skybox/left.jpg ./original_assets/skybox/top.jpg ./original_assets/skybox/bottom.jpg ./original_assets/skybox/front.jpg ./original_assets/skybox/back.jpg -output_file ./assets/skybox_xuastc_ldr_8x8_cubemap_mips.basisu.ktx2
+$BASISU_TOOL -xuastc_ldr_6x6 -mipmap -cubemap ./original_assets/skybox/right.jpg ./original_assets/skybox/left.jpg ./original_assets/skybox/top.jpg ./original_assets/skybox/bottom.jpg ./original_assets/skybox/front.jpg ./original_assets/skybox/back.jpg -output_file ./assets/skybox_xuastc_ldr_8x8_cubemap_mips.basisu.ktx2
 $BASISU_TOOL -astc_ldr_8x8 -mipmap ./original_assets/kodim20.png -output_file ./assets/kodim20_astc_ldr_8x8_mips.basisu.ktx2
 $BASISU_TOOL -uastc_ldr_4x4 ./original_assets/tough_fixed.png -output_file ./assets/tough_uastc_ldr_4x4.basisu.ktx2
-$BASISU_TOOL -xuastc_ldr_6x6 -mipmap ./original_assets/wikipedia_fixed_6x6.png -output_file ./assets/wikipedia_xuastc_ldr_6x6_mips.basisu.ktx2
+$BASISU_TOOL -xuastc_ldr_8x8 -mipmap ./original_assets/wikipedia_fixed_6x6.png -output_file ./assets/wikipedia_xuastc_ldr_8x8_mips.basisu.ktx2
