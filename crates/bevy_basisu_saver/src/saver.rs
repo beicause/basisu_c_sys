e8b99ea7acbf6f9226bb96752648057f29ec3c8f
Luo Zhihao <luo_zhihao@outlook.com>
2026-05-05 03:06:57 +0800

    Improve `BasisuSaverPlugin` (#41)

--- crates/bevy_basisu_saver/src/saver.rs
+++ crates/bevy_basisu_saver/src/saver.rs
@@ -1,61 +1,91 @@
+use basisu_c_sys::extra::{BasisuEncodeError, BasisuEncoder, BasisuEncoderParams};
 use bevy::{
-    asset::{AsyncWriteExt, saver::AssetSaver},
-    image::Image,
+    asset::{
+        AsyncWriteExt, processor::LoadTransformAndSave, saver::AssetSaver,
+        transformer::IdentityAssetTransformer,
+    },
+    image::{Image, ImageLoader},
     reflect::TypePath,
 };
 use bevy_basisu_loader::{BasisuLoader, BasisuLoaderSettings};
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
-use basisu_c_sys::extra::{BasisuEncodeError, BasisuEncoder, BasisuEncoderParams};
+/// Basis universal asset processor.
+pub type BasisuProcessor =
+    LoadTransformAndSave<ImageLoader, IdentityAssetTransformer<Image>, BasisuSaver>;
 
+/// Basis universal texture saver.
 #[derive(TypePath)]
-pub struct BasisuTextureSaver;
-
-#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
-pub struct BasisuTextureSaverSettings {
-    pub params: BasisuEncoderParams,
+pub struct BasisuSaver {
+    /// Default basisu encoder params.
+    /// See the documents and `BU_COMP_FLAGS_*` in [`basisu_c_sys`] if you want more controls,
+    /// like mipmap generation.
+    pub default_encoder_params: BasisuEncoderParams,
 }
 
-impl Default for BasisuTextureSaverSettings {
-    fn default() -> Self {
-        Self {
-            params: BasisuEncoderParams::new_with_srgb_defaults(
-                basisu_c_sys::BasisTextureFormat::XuastcLdr4x4,
-            ),
-        }
-    }
+/// Basis universal texture saver settings.
+#[derive(Debug, Clone, Copy, Default, Serialize, Deserialize)]
+pub struct BasisuSaverSettings {
+    /// Basisu encoder params. If it's None the [`BasisuSaver::default_encoder_params`] will be used.
+    pub encoder_params: Option<BasisuEncoderParams>,
 }
 
+/// An error when encoding an image using [`BasisuSaver`].
 #[derive(Debug, Error)]
 #[non_exhaustive]
-pub enum BasisuTextureSaverError {
+pub enum BasisuSaverError {
+    /// An error occurred while trying to load the bytes.
     #[error(transparent)]
     Io(#[from] std::io::Error),
+    /// An error occurred while trying to encode the image.
     #[error(transparent)]
     BasisuEncodeError(#[from] BasisuEncodeError),
 }
 
-impl AssetSaver for BasisuTextureSaver {
+impl AssetSaver for BasisuSaver {
     type Asset = Image;
-    type Settings = BasisuTextureSaverSettings;
+    type Settings = BasisuSaverSettings;
     type OutputLoader = BasisuLoader;
-    type Error = BasisuTextureSaverError;
+    type Error = BasisuSaverError;
 
     async fn save(
         &self,
         writer: &mut bevy::asset::io::Writer,
         asset: bevy::asset::saver::SavedAsset<'_, '_, Self::Asset>,
         settings: &Self::Settings,
-        _asset_path: bevy::asset::AssetPath<'_>,
+        asset_path: bevy::asset::AssetPath<'_>,
     ) -> Result<<Self::OutputLoader as bevy::asset::AssetLoader>::Settings, Self::Error> {
+        let _span = bevy::log::info_span!("Encoding basisu texture").entered();
+        let time = if log::STATIC_MAX_LEVEL >= log::LevelFilter::Debug {
+            Some(bevy::platform::time::Instant::now())
+        } else {
+            None
+        };
+
         let mut encoder = BasisuEncoder::new();
         encoder.set_image(basisu_c_sys::extra::SourceImage {
             data: asset.data.as_deref().unwrap_or(&[]),
             texture_descriptor: &asset.texture_descriptor,
             texture_view_descriptor: &asset.texture_view_descriptor,
         })?;
-        let result = encoder.compress(settings.params)?;
+        let result = encoder.compress(
+            settings
+                .encoder_params
+                .unwrap_or(self.default_encoder_params),
+        )?;
+
+        if log::STATIC_MAX_LEVEL >= log::LevelFilter::Debug {
+            bevy::log::debug!(
+                "Encoded basisu texture \"{}\", {}kb -> {}kb in {:?}",
+                asset_path,
+                asset.data.as_deref().unwrap_or(&[]).len() as f32 / 1000.0,
+                result.len() as f32 / 1000.0,
+                time.unwrap().elapsed(),
+            );
+        }
+        drop(_span);
+
         writer.write_all(&result).await?;
 
         Ok(BasisuLoaderSettings {
