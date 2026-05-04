19499e88873f6fb6a148033c6afbc65fd36cc8a0
Luo Zhiaho <luo_zhihao@outlook.com>
2026-05-05 01:38:16 +0800

    Improve `BasisuSaverPlugin`

--- crates/bevy_basisu_saver/src/lib.rs
+++ crates/bevy_basisu_saver/src/lib.rs
@@ -1,44 +1,42 @@
-//! A bevy asset processor to transform images to basisu ktx2 textures
-//!
-//! This is based on [basisu_c_sys](https://crates.io/crates/basisu_c_sys) and [bevy_basisu_loader](https://crates.io/crates/bevy_basisu_loader).
+//! Asset saver and processor for Basis Universal KTX2 textures.
 
-use crate::saver::BasisuTextureSaver;
-#[cfg(all(
-    target_arch = "wasm32",
-    target_vendor = "unknown",
-    target_os = "unknown",
-))]
-use bevy::platform::sync::{
-    Arc,
-    atomic::{AtomicUsize, Ordering},
-};
+extern crate alloc;
+
+use crate::saver::{BasisuProcessor, BasisuSaver};
+use alloc::borrow::Cow;
+use basisu_c_sys::extra::BasisuEncoderParams;
 use bevy::{
-    app::Plugin,
-    asset::{processor::LoadTransformAndSave, transformer::IdentityAssetTransformer},
-    image::{Image, ImageLoader},
-    prelude::*,
+    app::{App, Plugin},
+    image::ImageLoader,
 };
 use bevy_basisu_loader::BasisuLoaderPlugin;
 
 pub mod saver;
 pub use basisu_c_sys as sys;
 
-pub type BasisuProcessor =
-    LoadTransformAndSave<ImageLoader, IdentityAssetTransformer<Image>, BasisuTextureSaver>;
-
+/// Provides basis universal asset saver and processor.
 pub struct BasisuSaverPlugin {
-    /// The file extensions handled by the processor.
-    pub file_extensions: Vec<String>,
+    /// The file extensions handled by the basisu asset processor.
+    ///
+    /// Default is [`ImageLoader::SUPPORTED_FILE_EXTENSIONS`] except ktx2 and .dds.
+    pub processor_extensions: Vec<Cow<'static, str>>,
+    /// Default basisu encoder params.
+    /// See the documents and `BU_COMP_FLAGS_*` in [`basisu_c_sys`] if you want more controls,
+    /// like mipmap generation.
+    pub default_encoder_params: BasisuEncoderParams,
 }
 
 impl Default for BasisuSaverPlugin {
     fn default() -> Self {
         Self {
-            file_extensions: ImageLoader::SUPPORTED_FILE_EXTENSIONS
+            processor_extensions: ImageLoader::SUPPORTED_FILE_EXTENSIONS
                 .iter()
-                .filter(|s| !["basis", "ktx2", "dds"].contains(s))
-                .map(|s| s.to_string())
+                .filter(|s| !["ktx2", "dds"].contains(s))
+                .map(|s| Cow::Borrowed(*s))
                 .collect(),
+            default_encoder_params: BasisuEncoderParams::new_with_srgb_defaults(
+                basisu_c_sys::BasisTextureFormat::XuastcLdr4x4,
+            ),
         }
     }
 }
@@ -52,7 +50,7 @@ impl Default for BasisuSaverPlugin {
 struct BasisuWasmReady(Arc<AtomicUsize>);
 
 impl Plugin for BasisuSaverPlugin {
-    fn build(&self, app: &mut bevy::app::App) {
+    fn build(&self, app: &mut App) {
         #[cfg(all(
             target_arch = "wasm32",
             target_vendor = "unknown",
@@ -85,9 +83,14 @@ impl Plugin for BasisuSaverPlugin {
             .world()
             .get_resource::<bevy::asset::processor::AssetProcessor>()
         {
-            asset_processor.register_processor::<BasisuProcessor>(BasisuTextureSaver.into());
-            for ext in &self.file_extensions {
-                asset_processor.set_default_processor::<BasisuProcessor>(ext.as_str());
+            asset_processor.register_processor::<BasisuProcessor>(
+                BasisuSaver {
+                    default_encoder_params: self.default_encoder_params,
+                }
+                .into(),
+            );
+            for ext in &self.processor_extensions {
+                asset_processor.set_default_processor::<BasisuProcessor>(ext);
             }
         }
     }
